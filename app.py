74c7dcdf11ed68bf87abedda46520c56e4320ffe
Unfinishedgod <shjj08@gmail.com>
2024-02-16 09:48:15 +0000

    modify app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 topics = [
     {'id':1, 'title': 'html', 'body': 'html is ...'},
     {'id':2, 'title': 'css', 'body': 'css is ...'},
-    {'id':3, 'title': 'javascript', 'body': 'javascript is ...'}
+    {'id':3, 'title': 'javascript_main', 'body': 'javascript is ...'}
 ]
 
 def template(contents, content):
